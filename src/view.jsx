20a8df19dddfee85aeebf883a26c8e1c4986aa1f
Liang Xingchen <liang@maichong.it>
2016-03-21 23:58:11 +0800

    bootstrap

--- src/view.jsx
+++ src/view.jsx
@@ -5,81 +5,24 @@
  */
 
 import React from 'react';
-import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
-import ContextPure from 'material-ui/lib/mixins/context-pure';
 import Select from 'react-select';
 import 'react-select/dist/react-select.min.css';
 
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { actions } from 'alaska-admin-view';
+import qs from 'qs';
 
-import { shallowEqual } from 'alaska-admin-view';
+import { shallowEqual, api, PREFIX } from 'alaska-admin-view';
 
-class RelationshipFieldView extends React.Component {
+export default class RelationshipFieldView extends React.Component {
 
   static propTypes = {
     children: React.PropTypes.node
   };
 
-  static contextTypes = {
-    muiTheme: React.PropTypes.object,
-    views: React.PropTypes.object,
-    settings: React.PropTypes.object
-  };
-
-  static childContextTypes = {
-    muiTheme: React.PropTypes.object,
-    views: React.PropTypes.object,
-  };
-
-  static mixins = [
-    ContextPure
-  ];
-
-  constructor(props, context) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSearch = this.handleSearch.bind(this);
-    this.state = {
-      muiTheme: context.muiTheme ? context.muiTheme : getMuiTheme(),
-      views: context.views
-    };
-  }
-
-  getChildContext() {
-    return {
-      muiTheme: this.state.muiTheme,
-      views: this.context.views,
-    };
-  }
-
-  componentWillReceiveProps(nextProps, nextContext) {
-    let newState = {};
-    if (nextContext.muiTheme) {
-      newState.muiTheme = nextContext.muiTheme;
-    }
-    if (nextContext.views) {
-      newState.views = nextContext.views;
-    }
-    this.setState(newState, () => {
-      if (this._searchCallback && this.props.search) {
-        let search = this.props.search;
-        let field = this.props.field;
-        let key = field.service + '-' + field.model;
-        if (search[key] && search[key][this._searchKeyword]) {
-          this._searchCallback(null, { options: search[key][this._searchKeyword] });
-          this._searchCallback = null;
-        }
-      }
-    });
-  }
-
   shouldComponentUpdate(props) {
     return !shallowEqual(props, this.props, 'data', 'onChange', 'search');
   }
 
-  handleChange(value) {
+  handleChange = (value) => {
     if (this.props.onChange) {
       let val = '';
       if (this.props.field.multi) {
@@ -92,71 +35,41 @@ class RelationshipFieldView extends React.Component {
       }
       this.props.onChange(val);
     }
-  }
+  };
 
-  _searchCallback = null;
-  _searchKeyword = '';
-
-  handleSearch(keyword, callback) {
-    let { search, actions, field } = this.props;
-    let key = field.service + '-' + field.model;
-    if (search[key] && search[key][keyword]) {
-      callback(null, { options: search[key][keyword] });
-      return;
-    }
-    actions.search({
+  handleSearch = (keyword, callback) => {
+    let field = this.props.field;
+    let query = qs.stringify({
       service: field.service,
       model: field.model,
-      keyword
+      search: keyword
     });
-    this._searchCallback = callback;
-    this._searchKeyword = keyword;
-  }
+    api.post(PREFIX + '/api/search?' + query).then(res => {
+      callback(null, { options: res.results });
+    }, callback);
+  };
 
   render() {
-    let { field, value, model, disabled, errorText } = this.props;
-    let { muiTheme } = this.state;
-    let noteElement = field.note ?
-      <div style={field.fullWidth?muiTheme.fieldNote:muiTheme.fieldNoteInline}>{field.note}</div> : null;
-    let styles = {
-      root: {
-        padding: '10px 0'
-      },
-      label: {
-        fontSize: '12px',
-        color: '#999',
-        paddingBottom: 8
-      },
-      select: {
-        width: '100%',
-        marginBottom: 5
-      },
-      error: {
-        color: muiTheme.textField.errorColor,
-        fontSize: 12
-      }
-    };
-    let errorLabel = errorText ? <div style={styles.error}>{errorText}</div> : null;
+    let { field, value, disabled, errorText } = this.props;
+    let noteElement = field.note ? <p className="help-block">{field.note}</p> : null;
+    let errorLabel = errorText ? <p className="help-block text-danger">{errorText}</p> : null;
     return (
-      <div style={styles.root}>
-        <div style={styles.label}>{field.label}</div>
-        <Select.Async
-          multi={field.multi}
-          value={value}
-          style={styles.select}
-          disabled={disabled}
-          valueKey="_id"
-          labelKey="title"
-          onChange={this.handleChange}
-          loadOptions={this.handleSearch}
-        />
-        {noteElement}
-        {errorLabel}
+      <div className="form-group">
+        <label className="control-label col-xs-2">{field.label}</label>
+        <div className="col-xs-10">
+          <Select.Async
+            multi={field.multi}
+            value={value}
+            disabled={disabled}
+            valueKey="_id"
+            labelKey="title"
+            onChange={this.handleChange}
+            loadOptions={this.handleSearch}
+          />
+          {noteElement}
+          {errorLabel}
+        </div>
       </div>
     );
   }
 }
-
-export default connect(({ search }) => ({ search }), dispatch => ({
-  actions: bindActionCreators(actions, dispatch)
-}))(RelationshipFieldView);
