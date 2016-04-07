1163fee747a86dcadc560b6286aa305e95c6a3de
Liang Xingchen <liang@maichong.it>
2016-04-07 10:37:27 +0800

    0.7.0

--- index.js
+++ index.js
@@ -19,36 +19,61 @@ class RelationshipField extends alaska.Field {
   initSchema() {
     let schema = this._schema;
     let model = this._model;
-    let type = TypeObjectId;
-    if (typeof this.ref === 'string') {
-      this.ref = model.service.model(this.ref);
+    let dataType = this.dataType;
+    if (dataType === 'ObjectId' || dataType === ObjectId) {
+      dataType = TypeObjectId;
     }
     let ref = this.ref;
-    if (ref.fields._id) {
-      type = ref.fields._id;
-      if (type.type) {
-        type = type.type;
-      }
-      if (type.plain) {
-        type = type.plain;
-      }
+    if (typeof ref === 'string') {
+      //查找引用模型
+      //ref 当this.optional为true时,ref有可能为null
+      ref = model.service.model(this.ref, this.optional);
     }
 
-    if (ref.registered) {
-      //引用已经注册的模型
-      this.service = this.ref.service.id;
+    let options;
+    let type;
+    if (ref) {
+      //找到了引用模型
+      if (dataType) {
+        type = dataType;
+      } else if (ref.fields._id) {
+        type = ref.fields._id;
+        if (type.type) {
+          type = type.type;
+        }
+        if (type.plain) {
+          type = type.plain;
+        }
+      }
+      if (!type) {
+        type = TypeObjectId;
+      }
+      options = {
+        type,
+        ref: ref.name
+      };
+
+      if (ref.registered) {
+        //引用已经注册的模型
+        this.service = ref.service.id;
+      } else {
+        //引用还未注册的模型
+        //只有与当前模型处在同一Service时才会发生
+        //所以,直接取当前模型Service
+        this.service = this._model.service.id;
+      }
+      this.model = ref.name;
     } else {
-      //引用还未注册的模型
-      //只有与当前模型处在同一Service时才会发生
-      //所以,直接取当前模型Service
-      this.service = this._model.service.id;
+      //如果没有找到引用,说明是可选引用
+      this.hidden = true;
+      type = dataType || TypeObjectId;
+      options = type;
+      if (typeof this.ref === 'string') {
+        let arr = this.ref.split('.');
+        this.model = arr[1];
+        this.service = arr[0];
+      }
     }
-    this.model = ref.name;
-
-    let options = {
-      type,
-      ref: this.ref.name
-    };
 
     [
       'get',
@@ -66,6 +91,7 @@ class RelationshipField extends alaska.Field {
     if (this.multi) {
       options = [options];
     }
+    this.ref = ref;
     this.dataType = type;
     schema.path(this.path, options);
   }
@@ -115,8 +141,10 @@ RelationshipField.plain = TypeObjectId;
 
 RelationshipField.viewOptions = ['filters', 'service', 'model', 'multi', function (options, field) {
   let Model = field.ref;
-  options.key = Model.key;
-  options.title = Model.title;
+  if (Model) {
+    options.key = Model.key;
+    options.title = Model.title;
+  }
 }];
 
 module.exports = RelationshipField;
